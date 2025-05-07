099e4ecfcb6fecf3e6a9bd981f4f8d8ee600e128
Iryna Shevkun <sevkunirina65@gmail.com>
2025-05-07 16:55:17 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,36 @@
 'use strict';
 
+const wall = document.querySelector('div[class="wall"]');
+const spider = wall.querySelector('img[class="spider"]');
+
+const wallCoords = wall.getBoundingClientRect();
+const wallBorder = wall.clientLeft;
+const spiderSize = parseInt(getComputedStyle(spider).height);
+
 document.addEventListener('click', (e) => {
-  // write code here
+  if (!wall.contains(e.target)) {
+    return;
+  }
+
+  const clickY = e.clientY - wallCoords.top - wallBorder;
+  const clickX = e.clientX - wallCoords.left - wallBorder;
+
+  const spiderX = Math.max(
+    0,
+    Math.min(
+      clickX - spiderSize / 2,
+      wallCoords.width - spiderSize - wallBorder * 2,
+    ),
+  );
+
+  const spiderY = Math.max(
+    0,
+    Math.min(
+      clickY - spiderSize / 2,
+      wallCoords.height - spiderSize - wallBorder * 2,
+    ),
+  );
+
+  spider.style.top = `${spiderY}px`;
+  spider.style.left = `${spiderX}px`;
 });
